60a6737e32fd35b4df8759ee35325686a2603e2d
Jordan Kaye <jordan.kaye2@gmail.com>
2018-10-23 02:31:59 +0000

    Sketching some API ideas, nothing concrete yet

--- src/Network/Ipfs/Core.hs
+++ src/Network/Ipfs/Core.hs
@@ -8,6 +8,9 @@ module Network.Ipfs.Core
   ) where
 
 import qualified Data.ByteString.Lazy as B
+import qualified Data.Set as S
+import qualified Data.Text as T
+import Data.Binary (Binary)
 import Data.Binary.Builder (Builder, fromLazyByteString, append)
 import Data.ByteString.Conversion (toByteString)
 import Network.HTTP.Types
@@ -50,3 +53,59 @@ apiRoot (IpfsConnectionInfo{..}) =
       version = apiVersionUriPart ipfsVersion
   in
     fromLazyByteString $ B.concat [https, ipfsHost, ":", port, "/api/", version]
+
+-- data IpfsOperation = OpSwarmPeers
+--                    | OpBootstrapList
+--                    deriving (Show)
+
+-- pathSegments :: IpfsOperation -> [T.Text]
+-- pathSegments OpSwarmPeers = ["swarm", "peers"]
+-- pathSegments OpBootstrapList = ["bootstrap", "list"]
+
+newtype IpfsQueryItem = IpfsQueryItem { getQueryItem :: QueryItem }
+  deriving (Show)
+
+instance Eq IpfsQueryItem where
+  a == b = let (ak, _) = getQueryItem a
+               (bk, _) = getQueryItem b
+           in
+             ak == bk
+
+instance Ord IpfsQueryItem where
+  a <= b = let (ak, _) = getQueryItem a
+               (bk, _) = getQueryItem b
+           in
+             ak <= bk
+
+type PathSegments = [T.Text]
+type IpfsQuery = S.Set IpfsQueryItem
+
+data IpfsHttpInfo = IpfsHttpInfo PathSegments IpfsQuery
+  deriving (Show)
+
+class IpfsOperation a where
+  toHttpInfo :: a -> IpfsHttpInfo
+
+-- Need to encode return value in this as well so that reponses can be properly marshalled.
+-- I'm thinking that there must be some way to not have to write the boilerplate that's been
+-- included in the implementation below... It's clearly very mechanical, but I haven't quite
+-- been able to figure out where the right abstraction is to make things work cleanly.
+data OpSwarmPeers = OpSwarmPeers IpfsQuery
+  deriving (Show)
+
+updateQuery :: IpfsQuery -> QueryItem -> IpfsQuery
+updateQuery i q = S.insert (IpfsQueryItem q) i
+
+verbose :: OpSwarmPeers -> OpSwarmPeers
+verbose (OpSwarmPeers q) = OpSwarmPeers $ updateQuery q ("verbose", Nothing) 
+
+withLatency :: OpSwarmPeers -> OpSwarmPeers
+withLatency (OpSwarmPeers q) = OpSwarmPeers $ updateQuery q ("latency", Nothing)
+
+withStreams :: OpSwarmPeers -> OpSwarmPeers
+withStreams (OpSwarmPeers q) = OpSwarmPeers $ updateQuery q ("streams", Nothing)
+
+instance IpfsOperation OpSwarmPeers where
+  toHttpInfo (OpSwarmPeers q) = IpfsHttpInfo ["swarm", "peers"] q
+
+  

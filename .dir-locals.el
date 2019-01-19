45a24d70ae23a5ca58b3799d932dba5bc38c5cdf
Jordan Kaye <jordan.kaye2@gmail.com>
2019-01-19 14:19:04 +0000

    Initial testing infrastructure

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "hs-ipfs-api:lib" "hs-ipfs-api:exe:hs-ipfs-api-exe" "hs-ipfs-api:test:hs-ipfs-api-test")))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Control.Concurrent
+import System.Process
+import Test.Hspec
+
+import Network.Ipfs.Core
+import Network.Ipfs.Bootstrap
+
+ignoreHookContext :: SpecWith () -> SpecWith a
+ignoreHookContext = aroundWith (\actionRunner -> const (actionRunner ()))
+
+startIpfsDaemon :: IO ProcessHandle
+startIpfsDaemon = do
+  handle <- spawnCommand "ipfs daemon"
+  threadDelay 5000000
+  return handle
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ beforeAll startIpfsDaemon . afterAll terminateProcess $ do
+  ignoreHookContext $ do
+    describe "thing" $ do
+      it "is a test" $ do
+        body <- performIpfsOperation defaultConnectionInfo OpBootstrapList
+        True `shouldBe` True
